6ad5cc70549b181e6789409b18be38a11f8e733c
Harsh Bhansali <harshbhansali@harshs-air.lan>
2023-09-13 09:59:01 -0400

    Updated SQL

--- init.sql
+++ init.sql
@@ -1,7 +1,5 @@
-CREATE SEQUENCE IF NOT EXISTS feedback_id_seq START 1;
-
 CREATE TABLE IF NOT EXISTS feedback (
-    id integer NOT NULL DEFAULT nextval('feeback_id_seq'::regclass),
+    id SERIAL NOT NULL,
     datetimestamp text NOT NULL,
     teacher_name text NOT NULL,
     activity_covered text NOT NULL,
